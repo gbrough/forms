93c43d7be31fda40f210e9aa35250ed7ccdf1bb7
gbrough <gbrough@gmail.com>
2021-06-02 19:07:35 -0700

    updated list on form

--- js/scripts3.js
+++ js/scripts3.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
     event.preventDefault();
     const person1Input = $("input#person1").val();
     const person2Input = $("input#person2").val();
-    const jewelryInput = $("#input#jewelry").val();
-    const jewelry = $("#jewelry").val();
+    const jewelryInput = $("select#jewelry").val();
+
 
     $(".person1").text(person1Input);
     $(".person2").text(person2Input);
